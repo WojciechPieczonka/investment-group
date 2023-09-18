aad2ac3776d06f93d054623705dffe9198bc4cd4
Wojciech Pieczonka <wojciechpieczonka@gmail.com>
2023-09-18 19:13:51 +0200

    starting work on menu mechanics

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 const navMobile = document.querySelector('.nav-mobile');
+const navBtn = document.querySelector('.hamburger');
+const footerYear = document.querySelector('.footer__year');
 
-// nav-mobile-avtive
+const handleNav = () => {
+	navBtn.classList.add('is--active');
+	navMobile.classList.toggle('nav-mobile--active');
+};
+
+navBtn.addEventListener('click', handleNav);
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+handleCurrentYear();
