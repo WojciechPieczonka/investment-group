dfee517e32c039426b1f36a89a741683637d816d
Wojciech Pieczonka <wojciechpieczonka@gmail.com>
2023-09-19 19:14:14 +0200

    js script done

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer__year');
+const footer = document.querySelector('.footer');
 
 const handleNav = () => {
-	navBtn.classList.add('is--active');
+	navBtn.classList.toggle('is-active');
 	navMobile.classList.toggle('nav-mobile--active');
+	if (navMobile.classList.contains('nav-mobile--active')) {
+		footer.style.display = 'none';
+	} else {
+		footer.style.display = 'block';
+	}
 };
 
 navBtn.addEventListener('click', handleNav);
